b2eddcaebc4d167667a58832d94d2342449d27a3
levim <levi.miller1497@gmail.com>
2025-10-16 21:11:01 -0400

    Here we go...
    Part One: Button Factor
    // created new branch from existing main branch

---
+++ SWE_App/ButtonFactory.h
@@ -0,0 +1,12 @@
+#pragma once
+// Part One: Button Factory
+// Create a new branch from existing main branch
+// On the new branch, create a new class called, ButtonFactory (methods may be static)
+// Inside ButtonFactory, create functions that facilitate creating each button,
+// should have one method for generically creating buttons based off of parameters and multiple methods for each
+// Change logic in main calculator app to use ButtonFactory
+
+class ButtonFactory
+{
+};
+
